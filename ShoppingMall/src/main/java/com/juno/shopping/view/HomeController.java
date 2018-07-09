424acd9cb94968f19aa2572c7f4d161e8e1c296c
startimbre <startimbre@gmail.com>
2018-07-09 14:46:44 +0900

    2018.7.9 상세보기 이미지 변경중

--- ShoppingMall/src/main/java/com/juno/shopping/view/HomeController.java
+++ ShoppingMall/src/main/java/com/juno/shopping/view/HomeController.java
@@ -1,16 +1,26 @@
 package com.juno.shopping.view;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.juno.shopping.biz.product.ProductService;
+import com.juno.shopping.biz.product.ProductVO;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+//@SessionAttributes("product")
 public class HomeController {
+	@Autowired
+	private ProductService productService;
 	
 	@RequestMapping(value = "index.do")
-	public String index() {
+	public String index(ProductVO vo, Model model) {
+		model.addAttribute("productList", productService.getProductList(vo)); 
 		return "index";
 	}
 	
@@ -19,10 +29,10 @@ public class HomeController {
 		return "shop";
 	}
 	
-	@RequestMapping(value = "product-details.do")
-	public String productDetail() {
-		return "product-details";
-	}
+//	@RequestMapping(value = "product-details.do")
+//	public String productDetail() {
+//		return "product-details";
+//	}
 	
 	@RequestMapping(value = "cart.do")
 	public String cart() {
